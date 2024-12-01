6544eac46105352fa73c9f471a81c998d31d4ccb
Chris Cooper <cjcooper252@gmail.com>
2024-12-01 12:34:27 -0800

    Solve 1.1

--- src/01/1.js
+++ src/01/1.js
@@ -1,5 +1,27 @@
-module.exports = function (input) {
+function parseInput(input) {
   const lines = input.trim().split("\n");
+  const firstList = [];
+  const secondList = [];
 
-  console.log("Input lines:", lines);
+  for (const line of lines) {
+    const [firstNumber, secondNumber] = line.split("   ").map(Number);
+    firstList.push(firstNumber);
+    secondList.push(secondNumber);
+  }
+
+  return { firstList: firstList.sort(), secondList: secondList.sort() };
+}
+
+module.exports = function (input) {
+  // Parse the input into 2 lists
+  // Sort each list
+  const { firstList, secondList } = parseInput(input);
+
+  // Loop over list and sum differences
+  const result = firstList.reduce((sum, current, index) => {
+    const diff = Math.abs(firstList[index] - secondList[index]);
+    return sum + diff;
+  }, 0);
+
+  console.log(result);
 };
